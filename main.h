d42df1cd00c935b13746dcc5ebfcfed5b6927f78
Magdaline <Keeshawamboh@gmail.com>
2023-05-13 14:53:38 -0700

    Added the main.h file with the prototype functions

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fentl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+#endif
